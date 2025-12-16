a29bea490ba4d2884cb326488ec33cb3ce7f31c6
Mikita Ihnatau <streettyymax@gmail.com>
2025-12-16 15:46:47 +0300

    First try to commit a handshake

---
+++ internal/message/message.go
@@ -0,0 +1,73 @@
+package message
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+type messageID uint8
+
+const (
+	MsgChoke         messageID = 0 // Means the peer isn't ready to accept messages
+	MsgUnchoke       messageID = 1 // Peer is unchoked
+	MsgInterested    messageID = 2
+	MsgNotInterested messageID = 3
+	MsgHave          messageID = 4
+	MsgBitfield      messageID = 5
+	MsgRequest       messageID = 6
+	MsgPiece         messageID = 7
+	MsgCancel        messageID = 8
+)
+
+// A message starts with a length indicator which tells us how many bytes long the message will be.
+// It’s a 32-bit integer, meaning it’s made out of four bytes smooshed together in big-endian order.
+// Message strores ID and payload of a message
+type Message struct {
+	ID      messageID
+	Payload []byte
+}
+
+// Function to serialize a message struct to form
+// <length_of_a_message><id><optional_payload>
+func (m *Message) Serialize() []byte {
+	// nil is keep-alive message
+	if m == nil {
+		return make([]byte, 4)
+	}
+
+	length := uint32(len(m.Payload) + 1) // + 1 for ID
+	buf := make([]byte, 4+length)
+
+	binary.BigEndian.PutUint32(buf[0:4], length)
+	buf[4] = byte(m.ID)
+	copy(buf[5:], m.Payload)
+
+	return buf
+}
+
+func Read(r io.Reader) (*Message, error) {
+	lengthBuf := make([]byte, 4)
+	_, err := io.ReadFull(r, lengthBuf)
+	if err != nil {
+		return nil, err
+	}
+	length := binary.BigEndian.Uint32(lengthBuf)
+
+	// keep-alive message
+	if length == 0 {
+		return nil, nil
+	}
+
+	messageBuf := make([]byte, length)
+	_, err = io.ReadFull(r, messageBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	m := Message{
+		ID:      messageID(messageBuf[0]),
+		Payload: messageBuf[1:],
+	}
+
+	return &m, nil
+}
